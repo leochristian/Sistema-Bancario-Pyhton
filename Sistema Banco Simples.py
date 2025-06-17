4174bf83caeb1b820866099c93ede6b30f127516
Leo Dallmann <leodalman92@gmail.com>
2025-06-16 22:58:14 -0300

    Create Sistema Banco Simples.py

---
+++ Sistema Banco Simples.py
@@ -0,0 +1,69 @@
+menu = """
+
+[d] Depositar
+[s] Sacar
+[e] Extrato
+[q] Sair
+=> """
+
+saldo = 0
+limite = 500
+extrato = ""
+numero_saques = 0
+LIMITE_SAQUES = 3
+
+while True:
+
+    opcao = input(menu)
+
+    if opcao == "d":
+        valor = float(input("Informe o valor a ser depositado: "))
+
+        if valor > 0:
+            saldo += valor
+            extrato += f"Depósito: R$ {valor:.2f}\n"
+
+        else:
+            print("Operação falhou! O valor informado é inválido.")
+
+    elif opcao == "s":
+        if saldo == 0:
+            print("Saldo Insuficiente para saque!! Efetue um depósito!")
+            
+        else:
+            valor = float(input("Informe o valor a ser sacado: "))
+
+            excedeu_saldo = valor > saldo
+
+            excedeu_limite = valor > limite
+
+            excedeu_saques = numero_saques >= LIMITE_SAQUES
+
+            if excedeu_saldo:
+                print("Operação falhou! Saldo insuficiente")
+
+            elif excedeu_limite:
+                print("Operação falhou! Limite de R$\033[1m500\033[0m para saque excedido.")
+
+            elif excedeu_saques:
+                print("Operação falhou! Excedeu número de saques.")
+
+            elif valor > 0:
+                saldo -= valor
+                extrato += f"Saque: R$ {valor:.2f}\n"
+                numero_saques += 1
+
+            else:
+                print("Operação falhou! O valor informado é inválido.")
+
+    elif opcao == "e":
+        print("\n================ EXTRATO ================")
+        print("Não foram realizadas movimentações." if not extrato else extrato)
+        print(f"\nSaldo: R$ {saldo:.2f}")
+        print("==========================================")
+
+    elif opcao == "q":
+        break
+
+    else:
+        print("Operação inválida, Selecione novamente a opçao do menu.")
